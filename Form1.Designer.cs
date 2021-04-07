485e985c638b1c2af1c388a921110aac7f4b9b70
Aziz Hasan <abdulaziz.mhus@gmail.com>
2021-04-08 00:24:17 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace GardenGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.ProcOpenLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gardenCBox = new System.Windows.Forms.CheckBox();
+            this.playerNameLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // ProcOpenLabel
+            // 
+            this.ProcOpenLabel.AutoSize = true;
+            this.ProcOpenLabel.Location = new System.Drawing.Point(95, 9);
+            this.ProcOpenLabel.Name = "ProcOpenLabel";
+            this.ProcOpenLabel.Size = new System.Drawing.Size(35, 13);
+            this.ProcOpenLabel.TabIndex = 1;
+            this.ProcOpenLabel.Text = "label1";
+            this.ProcOpenLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Game Status : ";
+            // 
+            // gardenCBox
+            // 
+            this.gardenCBox.AutoSize = true;
+            this.gardenCBox.Checked = true;
+            this.gardenCBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.gardenCBox.Location = new System.Drawing.Point(15, 66);
+            this.gardenCBox.Name = "gardenCBox";
+            this.gardenCBox.Size = new System.Drawing.Size(96, 17);
+            this.gardenCBox.TabIndex = 3;
+            this.gardenCBox.Text = "Gardening Shit";
+            this.gardenCBox.UseVisualStyleBackColor = true;
+            // 
+            // playerNameLabel
+            // 
+            this.playerNameLabel.AutoSize = true;
+            this.playerNameLabel.Location = new System.Drawing.Point(95, 22);
+            this.playerNameLabel.Name = "playerNameLabel";
+            this.playerNameLabel.Size = new System.Drawing.Size(35, 13);
+            this.playerNameLabel.TabIndex = 4;
+            this.playerNameLabel.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(270, 95);
+            this.Controls.Add(this.playerNameLabel);
+            this.Controls.Add(this.gardenCBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ProcOpenLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Gardening Shit";
+            this.TopMost = true;
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label ProcOpenLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox gardenCBox;
+        private System.Windows.Forms.Label playerNameLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Memory;
+using AutoIt;
+
+namespace GardenGUI
+{
+    
+    public partial class Form1 : Form
+    {
+        public Mem m = new Mem();
+        public bool ProcOpen = false;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Process[] processCollection = Process.GetProcessesByName("ffxiv_dx11");
+            
+            // constantly check if the process is available and open
+            ProcOpen = m.OpenProcess("ffxiv_dx11");
+            Thread.Sleep(100);
+            backgroundWorker1.ReportProgress(0); // do UI thread stuff
+        }
+
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if (ProcOpen)
+            {
+                ProcOpenLabel.ForeColor = Color.Green;
+                playerNameLabel.ForeColor = Color.Green;
+                ProcOpenLabel.Text = "Game Found";
+                playerNameLabel.Text = m.ReadString("base+1DB5789");
+                gardenCBox.Enabled = true;
+            }
+            else
+            {
+                ProcOpenLabel.ForeColor = Color.Red;
+                ProcOpenLabel.Text = "N/A";
+                gardenCBox.Enabled = false;
+                return; // do not continue if process is not available/open
+            }
+
+            if (gardenCBox.Checked && gardenCBox.Enabled)
+            {
+                string bubbleText = m.ReadString("base+01D69E68,C8,1F0,0");
+                // gardenCBox.Text = bubbleText;
+                byte[] arr1 = { 0 }; 
+                if (System.Convert.ToBoolean(m.ReadByte("base+1D8DE60")))
+                {
+                    if(bubbleText.Contains("is ready"))
+                    {
+                        AutoItX.Sleep(200);
+                        AutoItX.Send("{numpad0}");
+                        AutoItX.Sleep(100);
+                        AutoItX.Send("{numpad0}");
+                        AutoItX.Sleep(300);
+                    }
+
+                    if (bubbleText.Contains("is nothing"))
+                    {
+                        AutoItX.Sleep(200);
+                        short[] seq = { 0,0,0,0,6,0,0,2,4,0,4,0 };
+                        foreach (var item in seq)
+                        {
+                            AutoItX.Send("{numpad"+item+"}");
+                            AutoItX.Sleep(100);
+                        }
+                    }
+
+                    
+                    if(bubbleText.Contains("is doing"))
+                    {
+                        AutoItX.Sleep(200);
+                        AutoItX.Send("{numpad0}");
+                        AutoItX.Sleep(100);
+                        AutoItX.Send("{numpad2}");
+                        AutoItX.Sleep(100);
+                        AutoItX.Send("{numpad0}");
+                        AutoItX.Sleep(200);
+                    }
+                }
+            }
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
+        }
+    }
+}
